d6abe334f0fedc59234f9f0d4cfe4d7aa39bd02c
Aswanggaoda <70353367+Aswanggaoda@users.noreply.github.com>
2022-01-14 07:57:40 +0700

    update playlist.py

--- Yukki/Plugins/playlist.py
+++ Yukki/Plugins/playlist.py
@@ -38,7 +38,7 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton, InlineKeyboardM
 options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "all","16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",]   
 
 
-@Client.on_message(command(["playlist", "playlist@VeezMegaBot"]) & other_filters)
+@Client.on_message(command(["playlist", "playlist@Musichambot"]) & other_filters)
 async def pause_cmd(_, message):
     thumb ="cache/playlist.png"
     await message.reply_photo(
