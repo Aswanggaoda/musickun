c632a35944a9daa1ab4eeaaa88a8445138fa01c3
Aswanggaoda <70353367+Aswanggaoda@users.noreply.github.com>
2022-01-27 20:14:17 +0700

    update start.py

--- Yukki/Plugins/start.py
+++ Yukki/Plugins/start.py
@@ -37,7 +37,7 @@ pstart_markup=InlineKeyboardMarkup(
                         "add me to a Group", url="https://t.me/Musichambot?startgroup=true")
                 ],[
                     InlineKeyboardButton(
-                        "Commands", url="https://telegra.ph/Veez-Mega-Guide-01-10"),
+                        "Commands", url="https://telegra.ph/Hamusic-Bot-Guide-01-08"),
                     InlineKeyboardButton(
                         "setup Guide", url="https://telegra.ph/Veez-Mega-Guid-11-19")
                 ],[
