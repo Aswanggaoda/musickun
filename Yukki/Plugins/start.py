34255653d3366043e76148674c53739b56221e18
Aswanggaoda <70353367+Aswanggaoda@users.noreply.github.com>
2022-01-28 11:55:03 +0700

    update start.py

--- Yukki/Plugins/start.py
+++ Yukki/Plugins/start.py
@@ -21,7 +21,7 @@ from Yukki.YukkiUtilities.database.sudo import (get_sudoers, get_sudoers, remove
 def start_pannel():  
     buttons  = [
             [
-                InlineKeyboardButton(text="📚 Commands", url="https://telegra.ph/Veez-Mega-Guide-01-10")
+                InlineKeyboardButton(text="📚 Commands", url="https://telegra.ph/HAMUSIC-BOT-01-08")
             ],
             [ 
                 InlineKeyboardButton(text="📣 Channel", url="https://t.me/levinachannel"),
