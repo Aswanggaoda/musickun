a2bfc368f47542e6941f60dc1bbe7b74b64570c6
Aswanggaoda <70353367+Aswanggaoda@users.noreply.github.com>
2022-01-29 23:21:22 +0700

    Fix

--- Yukki/Plugins/ping.py
+++ Yukki/Plugins/ping.py
@@ -24,6 +24,7 @@ async def bot_sys_stats():
 async def ping(_, message):
     uptime = await bot_sys_stats()
     start = datetime.now()
+    response = await message.reply_text
     end = datetime.now()
     resp = (end - start).microseconds / 1000
     await response.edit_text(f"🏓 `PONG!`\n⚡ `{resp}` ms\n\n🖥 System Stats:\n{uptime}")  
