04df4b351af0586c26558de749c365fed65f071a
Aswanggaoda <70353367+Aswanggaoda@users.noreply.github.com>
2022-01-29 20:22:22 +0700

    update playlist.py

--- Yukki/Plugins/playlist.py
+++ Yukki/Plugins/playlist.py
@@ -40,9 +40,6 @@ options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", 
 
 @Client.on_message(command(["playlist", "playlist@Musichambot"]) & other_filters)
 async def pause_cmd(_, message):
-    thumb ="cache/playlist.png"
-    await message.reply_photo(
-    photo=thumb, 
     caption=("**❓ Which playlist do you want to play ?**"),    
     reply_markup=play_list_keyboard) 
     return 
